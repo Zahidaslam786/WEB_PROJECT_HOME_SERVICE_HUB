992cfb798269dc42636b7e5a75f10a754d292c8b
Muhammad Zahid <168932437+Zahidaslam786@users.noreply.github.com>
2025-05-14 23:38:26 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    // ... existing fields ...
+    resetPasswordOtp: String,
+    resetPasswordExpires: Date
+});
+
+// ... existing code ...
+
+module.exports = mongoose.model('User', userSchema);
 
